16610ae1767a8e91c4fe801a6ba27c685de499e0
unknown <mahdizolfaghari57@gmail.com>
2023-06-24 09:16:17 +0330

    swagger docs added

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 // connect DB
 const connectDB = require("./db/connect");
 
@@ -34,6 +39,11 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get('/' , (req,res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">API DOCS</a>')
+})
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use("/api/v1/auth", authRouter);
